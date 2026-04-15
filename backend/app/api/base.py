1ea55b9b1b17b010c2a0f2a64bf5f1912e72f759
Jiyao Li <jiyao.thomas.li@gmail.com>
2026-04-15 10:50:31 -0600

    center table entry

--- backend/app/api/base.py
+++ backend/app/api/base.py
@@ -8,17 +8,17 @@ router = APIRouter()
 @router.get("/mes")
 def get_mes(start:str, end:str):
     df = handle_weight_data(start, end)
-    return {"columns": [{ "field": col, "headerName": col, "width": 130 } for col in df.columns.to_list()],
+    return {"columns": [{ "field": col, "headerName": col, "width": 130, "align": 'center', "headerAlign": 'center', } for col in df.columns.to_list()],
             "content": df.to_dict(orient="records")}
 
 @router.get("/nau/time")
 def get_nau_time(start:str, end:str):
     df = handle_nau_time_data(start, end)
-    return {"columns": [{ "field": col, "headerName": col, "width": 130 } for col in df.columns.to_list()],
+    return {"columns": [{ "field": col, "headerName": col, "width": 130, "align": 'center', "headerAlign": 'center', } for col in df.columns.to_list()],
             "content": df.to_dict(orient="records")}
 
 @router.get("/nau/stop")
 def get_nau_stop(start:str, end:str):
     df = handle_nau_stop_data(start, end)
-    return {"columns": [{ "field": col, "headerName": col, "width": 130 } for col in df.columns.to_list()],
+    return {"columns": [{ "field": col, "headerName": col, "width": 130, "align": 'center', "headerAlign": 'center', } for col in df.columns.to_list()],
             "content": df.to_dict(orient="records")}
