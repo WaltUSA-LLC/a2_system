592c6c8d289d08160739eb11b940e3b2e70cdba8
Jiyao Li <jiyao.thomas.li@gmail.com>
2026-04-16 15:54:28 -0600

    fix mach eff error

--- backend/app/services/mach_view.py
+++ backend/app/services/mach_view.py
@@ -4,7 +4,27 @@ from nautilus_mes_base.utils import parse_start_date
 from app.services.utils import estimate_st_output_prs
 import pandas as pd
 import numpy as np
+import math
 
+def validate_throughput(rec:pd.Series) -> pd.Series:
+    nau = rec["NAU_prs"]
+    weight = rec["MES_prs"]
+    on_time = rec["ON_Time"]
+    avg_cycle = rec["Avg_Cycle"]
+    discard = rec["Discard_prs"]
+    theory_expect = math.ceil(on_time/avg_cycle/2) if not pd.isna(avg_cycle) else np.nan
+    act_expect = theory_expect - discard if not pd.isna(theory_expect) else np.nan
+    buffer = 10
+    
+    if (nau==0 and on_time==0) or (weight==0 and on_time==0):
+        return 0
+    if (weight<0 or weight>theory_expect+buffer or pd.isna(weight)) and (nau<0 or nau>theory_expect+buffer or pd.isna(nau)):
+        return np.nan
+    elif (weight<0 or weight>theory_expect+buffer or pd.isna(weight)):
+        return nau
+    else:
+        return weight
+        
 
 def handle_mach_view(start_time:str, end_time:str, shift:int)->pd.DataFrame:
     config = AppConfig.from_env()
@@ -20,7 +40,8 @@ def handle_mach_view(start_time:str, end_time:str, shift:int)->pd.DataFrame:
     df["Shift_Start_Time"] = df["Shift_Start_Time"].dt.strftime("%Y-%m-%d %H:%M:%S")
     df["ST_prs"] = df[["Avg_Cycle", "ON_Time", "OFF_Time"]].apply(estimate_st_output_prs, axis=1)
     df["ON_Time_Occupation"] = (df["ON_Time"] / (df["ON_Time"] + df["OFF_Time"])).round(2)
-    df["Mach_Efficiency"] = (df["MES_prs"]/df["ST_prs"]).round(2)
+    df["Real_prs"] = df[["NAU_prs", "ST_prs", "MES_prs", "ON_Time", "Discard_prs", "Avg_Cycle"]].apply(validate_throughput, axis=1)
+    df["Mach_Efficiency"] = (df["Real_prs"]/df["ST_prs"]).round(2)
     df.loc[df["ON_Time"]==0, "Mach_Efficiency"] = np.nan
     df["Comment"] = ""
     df.loc[df["Mach_Efficiency"] >= 0.8, "Comment"] = "Good"
