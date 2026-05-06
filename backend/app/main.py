ac6f72b1e5a7a5bc784b176f107ae64e20df9bde
Jiyao Li <jiyao.thomas.li@gmail.com>
2026-05-06 16:18:03 -0600

    add ip to cors origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5174",
-                   "http://localhost:5173",],
+                   "http://localhost:5173",
+                   "http://127.0.0.1:5173",
+                   "http://10.0.20.245:5173",
+                   "http://100.85.38.28:5173",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
