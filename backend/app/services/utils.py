2d5e7e3059611f86b16119a244a612b553efddd8
Jiyao Li <jiyao.thomas.li@gmail.com>
2026-06-04 15:01:19 -0600

    adjust mach on threshold

--- backend/app/services/utils.py
+++ backend/app/services/utils.py
@@ -5,26 +5,7 @@ import numpy as np
 from extractors import AppConfig
 from extractors import BaseExtractor
 from extractors.utils import parse_start_date
-
-# def validate_throughput(rec:pd.Series) -> pd.Series:
-#     nau = rec["NAU_prs"]
-#     weight = rec["MES_prs"]
-#     on_time = rec["ON_Time"]
-#     avg_cycle = rec["Avg_Cycle"]
-#     discard = rec["Discard_prs"]
-#     theory_expect = math.ceil(on_time/avg_cycle/2) if not pd.isna(avg_cycle) else np.nan
-#     act_expect = theory_expect - discard if not pd.isna(theory_expect) else np.nan
-#     buffer = 10
-    
-#     if (nau==0 and on_time==0) or (weight==0 and on_time==0):
-#         return 0
-#     if (weight<0 or weight>theory_expect+buffer or pd.isna(weight)) and (nau<0 or nau>theory_expect+buffer or pd.isna(nau)):
-#         return np.nan
-#     elif (weight<0 or weight>theory_expect+buffer or pd.isna(weight)):
-#         return nau
-#     else:
-#         return weight
-
+from cores.constants import ON_MACH_THRESHOLD
 
 def estimate_mes_output_prs(rec: pd.Series) -> int:
     if pd.isna(rec["Prs_Weight"]) or rec["Prs_Weight"] == 0:
@@ -52,7 +33,7 @@ def clean_weight(df: pd.DataFrame) -> pd.DataFrame:
 
 def filterShutdownMach(df: pd.DataFrame) -> pd.DataFrame:
     cleaned_df = df.copy()
-    cleaned_df = cleaned_df[(cleaned_df["Weight"]>0)|(cleaned_df["NAU_prs"]>0)|(cleaned_df["ON_Time"]>600)]
+    cleaned_df = cleaned_df[(cleaned_df["Weight"]>0)|(cleaned_df["NAU_prs"]>0)|(cleaned_df["ON_Time"]>ON_MACH_THRESHOLD)]
     return cleaned_df
 
 
