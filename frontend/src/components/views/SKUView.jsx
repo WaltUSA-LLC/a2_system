420237113e8719302e181aaa8a274f2326b59667
Jiyao Li <jiyao.thomas.li@gmail.com>
2026-07-06 14:38:27 -0600

    Bugfix: staff none makes diaglog crash

--- frontend/src/components/views/SKUView.jsx
+++ frontend/src/components/views/SKUView.jsx
@@ -136,10 +136,10 @@ function SKUView() {
                 setModalRec(records);
                 setMetaData({date_time: params.row.Shift_Start_Time, 
                     style:params.row.Style_Code,
-                    ko: staff[0].KO ? staff[0].KO : "None", 
-                    tech: staff[0].Tech ? staff[0].Tech : "None", 
-                    creeler: staff[0].Creeler ? staff[0].Creeler : "None", 
-                    yarner: staff[0].Yarner ? staff[0].Yarner : "None"
+                    ko: staff[0]?.KO ? staff[0].KO : "None", 
+                    tech: staff[0]?.Tech ? staff[0].Tech : "None", 
+                    creeler: staff[0]?.Creeler ? staff[0].Creeler : "None", 
+                    yarner: staff[0]?.Yarner ? staff[0].Yarner : "None"
                 })
             }
         ).catch((err) => {
